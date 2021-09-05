135f8c5c85acc9def97d036393d459af4a8ba69b
Mia Moretti <mimickal.dev@gmail.com>
2021-09-05 18:10:31 -0400

    Fun helper script for bumping versions

---
+++ build/release.js
@@ -0,0 +1,24 @@
+// This is not strictly part of the library. This is just a janky script that
+// bumps the release version and regenerates code docs.
+const fs = require('fs');
+const shell = require('child_process').execSync;
+
+const newversion = process.argv[2];
+if (!newversion) {
+	console.error('Missing version!');
+	process.exit(1);
+}
+
+function updateVersionForFile(file) {
+	const file_json = require(`../${file}`);
+	file_json.version = newversion;
+	fs.writeFileSync(file, JSON.stringify(file_json, null, 2));
+	console.log(`Wrote version ${newversion} to ${file}`);
+}
+
+updateVersionForFile('package.json');
+updateVersionForFile('package-lock.json');
+
+shell('git add package.json package-lock.json');
+shell(`git commit -m "Version bump to ${newversion}"`);
+
