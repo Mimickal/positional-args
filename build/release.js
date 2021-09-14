90087ff33ca76399f3c32c557aa96ae2be5ee88b
Mia Moretti <mimickal.dev@gmail.com>
2021-09-14 00:52:05 -0400

    Tweak build script to mention regenerating docs

--- build/release.js
+++ build/release.js
@@ -23,5 +23,5 @@ shell('npm run regen-docs');
 console.log('Wrote out new API.md');
 
 shell('git add package.json package-lock.json API.md');
-shell(`git commit -m "Version bump to ${newversion}"`);
+shell(`git commit -m "Version bump to ${newversion}, regen docs"`);
 
